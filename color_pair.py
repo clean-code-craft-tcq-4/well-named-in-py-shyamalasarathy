2d45709e5ffcad510546a76db5dfab2c7419d9ba
Shyamala Sarathy <105268170+shyamalasarathy@users.noreply.github.com>
2022-06-11 16:04:45 +0530

    Update color_pair.py

--- color_pair.py
+++ color_pair.py
@@ -1,10 +1,8 @@
 MAJOR_COLORS = ['White', 'Red', 'Black', 'Yellow', 'Violet']
 MINOR_COLORS = ["Blue", "Orange", "Green", "Brown", "Slate"]
 
-
 def color_pair_to_string(major_color, minor_color):
     return f'{major_color} {minor_color}'
-
 
 def get_color_from_pair_number(pair_number):
     zero_based_pair_number = pair_number - 1
@@ -15,7 +13,6 @@ def get_color_from_pair_number(pair_number):
     if minor_index >= len(MINOR_COLORS):
         raise Exception('Minor index out of range')
     return MAJOR_COLORS[major_index], MINOR_COLORS[minor_index]
-
 
 def get_pair_number_from_color(major_color, minor_color):
     try:
