392892f7f8bf9301d2c41c64b1ef877afb3e93ab
Shyamala Sarathy <105268170+shyamalasarathy@users.noreply.github.com>
2022-06-18 13:31:18 +0530

    Update and rename main.py to Main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-import testing
-
-if __name__ == '__main__':
-    testing.test_number_to_pair(4, 'White', 'Brown')
-    testing.test_number_to_pair(5, 'White', 'Slate')
-    testing.test_pair_to_number('Black', 'Orange', 12)
-    testing.test_pair_to_number('Violet', 'Slate', 25)
-    testing.test_pair_to_number('Red', 'Orange', 7)
-    print('Done :)')
